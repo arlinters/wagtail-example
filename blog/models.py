6924d6a1d8421e7a1d44ccd39c526653b2de00cc
arlinters <charlie.winters17@gmail.com>
2021-09-05 21:30:25 -0500

    end tutorial

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
+from django import forms
 from django.db import models
+from django.db.models.fields import Field
 
-from modelcluster.fields import ParentalKey
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 
@@ -9,6 +11,27 @@ from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
+
+from wagtail.snippets.models import register_snippet
+
+@register_snippet
+class BlogCategory(models.Model):
+	name = models.CharField(max_length=255)
+	icon = models.ForeignKey(
+		'wagtailimages.Image', null=True, blank=True,
+		on_delete=models.SET_NULL, related_name='+'
+	)
+
+	panels = [
+		FieldPanel('name'),
+		ImageChooserPanel('icon'),
+	]
+
+	def __str__(self):
+		return self.name
+	
+	class Meta:
+		verbose_name_plural = 'blog cateogires'
 
 class BlogIndexPage(Page):
 	intro = RichTextField(blank=True)
@@ -50,6 +73,7 @@ class BlogPage(Page):
 	intro = models.CharField(max_length=250)
 	body = RichTextField(blank=True)
 	tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+	categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
 
 	def main_image(self):
 		gallery_item = self.gallery_images.first()
@@ -66,7 +90,8 @@ class BlogPage(Page):
 	content_panels = Page.content_panels + [
 			MultiFieldPanel([
 				FieldPanel('date'),
-				FieldPanel('tags')
+				FieldPanel('tags'),
+				FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
 			], heading="Blog Information"),
 			InlinePanel('gallery_images', label='Gallery images'),
 			FieldPanel('intro'),
