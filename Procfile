0390810e7790de9afe346bc75a121bd029f7b238
arlinters <charlie.winters17@gmail.com>
2021-09-07 19:36:21 -0500

    add heroku dependencies

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wagtail-example.wsgi --log-file -
--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,4 +1,6 @@
+from __future__ import absolute_import, unicode_literals
 from .base import *
+import os
 
 DEBUG = False
 
@@ -6,3 +8,12 @@ try:
     from .local import *
 except ImportError:
     pass
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
